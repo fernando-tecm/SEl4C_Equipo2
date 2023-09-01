c568f54de44845171397f67f90f2f1c82cba505b
fernando-tecm <A01642285@tec.mx>
2023-08-31 19:45:54 -0600

    Initial Commit

---
+++ sel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sel
+//
+//  Created by Fernando García on 31/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
