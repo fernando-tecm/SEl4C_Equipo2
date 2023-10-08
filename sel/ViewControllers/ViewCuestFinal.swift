11b5d221ad716e187976ebb981c839e6ec544319
Julia Pasten <A01660665@tec.mx>
2023-10-07 19:31:36 -0600

    Vista NO TERMINADA DE CUESTIONARIO FINAL

---
+++ sel/ViewControllers/ViewCuestFinal.swift
@@ -0,0 +1,36 @@
+//
+//  ViewCuestFinal.swift
+//  sel
+//
+//  Created by Julia🌺 Cascelli🌊 on 07/10/23.
+//
+
+import UIKit
+
+class ViewCuestFinal: UIViewController {
+
+    @IBAction func buttonTotalmenteDesacuerdo(_ sender: Any) {
+    }
+    
+
+    @IBAction func buttonDesacuerdo(_ sender: Any) {
+    }
+    @IBAction func buttonNiAcuerdoNiDesacuerdo(_ sender: Any) {
+    }
+    
+    @IBAction func buttonDeAcuerdo(_ sender: Any) {
+    }
+    @IBAction func buttonTotalmenteDeacuerdo(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- sel/ViewControllers/ViewPerfil.swift
+++ sel/ViewControllers/ViewPerfil.swift
@@ -36,6 +36,7 @@ class ViewPerfil: UIViewController {
         
     }
     
+    
     func estiloBotones(){
         formaView.layer.cornerRadius = 15
         
@@ -73,6 +74,34 @@ class ViewPerfil: UIViewController {
         labelNombre.text = loginVM.name
         labelPass.text = loginVM.password
     }
+    
+    @IBAction func modificarContraseña(_ sender: UIButton) {
+        // Verificar si la contraseña actual ingresada coincide con la contraseña actual en loginVM
+        if let currentPassword = labelPass.text, currentPassword == loginVM.password {
+            // Verificar si la nueva contraseña y la confirmación coinciden
+            if let newPassword = labelNewPass.text, newPassword == labelConfirmNewPass.text {
+                // Actualizar la contraseña en loginVM
+                loginVM.password = newPassword
+                // Aquí puedes realizar cualquier otra acción que necesites, como guardar la contraseña en la base de datos, etc.
+                // Mostrar un mensaje de éxito
+                mostrarAlerta(mensaje: "Contraseña modificada exitosamente")
+            } else {
+                // Mostrar un mensaje de error si la nueva contraseña y la confirmación no coinciden
+                mostrarAlerta(mensaje: "Las contraseñas no coinciden")
+            }
+        } else {
+            // Mostrar un mensaje de error si la contraseña actual ingresada no es correcta
+            mostrarAlerta(mensaje: "Contraseña actual incorrecta")
+        }
+    }
 
-
+    // Función para mostrar una alerta
+    func mostrarAlerta(mensaje: String) {
+        let alertController = UIAlertController(title: "Mensaje", message: mensaje, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
+
+
